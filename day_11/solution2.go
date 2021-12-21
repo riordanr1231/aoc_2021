5fb7e96748781e6359333e422ef4e5faa798b9ff
Ryan Riordan <=>
2021-12-21 17:46:52 -0500

    day 11 solution 2

---
+++ day_11/solution2.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//go:embed input
+var input string
+var Lines []string
+
+type Octopus struct {
+	Energy  int
+	Flashed bool
+}
+
+type Octopuses struct {
+	Members [][]Octopus
+}
+
+var flashes int
+var octopuses Octopuses
+
+func init() {
+	InitInput()
+	InitOctopuses()
+}
+
+func InitInput() {
+	input = strings.TrimSuffix(input, "\n")
+	Lines = strings.Split(input, "\n")
+}
+
+func InitOctopuses() {
+	for _, l := range Lines {
+		var opus []Octopus
+		for _, b := range l {
+			val, _ := strconv.Atoi(string(b))
+			opus = append(opus, Octopus{val, false})
+
+		}
+		octopuses.Members = append(octopuses.Members, opus)
+	}
+}
+
+func (o *Octopus) Reset() {
+	o.Flashed = false
+	if o.Energy > 9 {
+		flashes++
+		o.Energy = 0
+	}
+}
+
+func (o *Octopus) IsEnergized() bool {
+	if o.Energy > 9 && !o.Flashed {
+		return true
+	}
+
+	return false
+}
+
+func (o *Octopus) Energize() {
+	(*o).Energy += 1
+}
+
+func (op *Octopuses) Neighbors(i int, j int) []*Octopus {
+	var nb []*Octopus
+
+	// horizontal and vertical
+	if j > 0 {
+		nb = append(nb, &op.Members[i][j-1])
+	}
+
+	if j != len(op.Members[0])-1 {
+		nb = append(nb, &op.Members[i][j+1])
+	}
+
+	if i != 0 {
+		nb = append(nb, &op.Members[i-1][j])
+	}
+
+	if i != len(op.Members)-1 {
+		nb = append(nb, &op.Members[i+1][j])
+	}
+
+	// diagonal
+	if i > 0 && j > 0 {
+		nb = append(nb, &op.Members[i-1][j-1])
+	}
+
+	if i != len(op.Members)-1 && j > 0 {
+		nb = append(nb, &op.Members[i+1][j-1])
+	}
+
+	if i > 0 && j != len(op.Members[0])-1 {
+		nb = append(nb, &op.Members[i-1][j+1])
+	}
+
+	if i != len(op.Members)-1 && j != len(op.Members[0])-1 {
+		nb = append(nb, &op.Members[i+1][j+1])
+	}
+
+	return nb
+}
+
+func (op *Octopuses) Print() {
+	for _, row := range op.Members {
+		for _, col := range row {
+			fmt.Print(col.Energy)
+		}
+		fmt.Println()
+	}
+}
+
+func (op *Octopuses) Round() {
+	for i := range op.Members {
+		for j := range op.Members[i] {
+			op.Members[i][j].Energize()
+		}
+	}
+
+	complete := false
+	for !complete {
+		complete = true
+		for i := range op.Members {
+			for j := range op.Members[i] {
+				if op.Members[i][j].IsEnergized() {
+					complete = false
+					op.Members[i][j].Flashed = true
+
+					nb := op.Neighbors(i, j)
+					for k := range nb {
+						nb[k].Energize()
+					}
+				}
+			}
+		}
+	}
+
+	for i := range op.Members {
+		for j := range op.Members[i] {
+			op.Members[i][j].Reset()
+		}
+	}
+}
+
+func (op *Octopuses) IsAllFlash() bool {
+	for i := range op.Members {
+		for j := range op.Members[i] {
+			if op.Members[i][j].Energy != 0 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	for i := 1; true; i++ {
+		octopuses.Round()
+		if octopuses.IsAllFlash() {
+			fmt.Println(i)
+			os.Exit(0)
+		}
+	}
+}
